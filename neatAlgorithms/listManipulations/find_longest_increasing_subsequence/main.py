6b75f7b8a12a49a805236820a1584b3bcefd80d4
Tutbjun <54132574+Tutbjun@users.noreply.github.com>
2021-10-24 16:43:12 +0200

    work on some algorithms

--- neatAlgorithms/listManipulations/find_longest_increasing_subsequence/main.py
+++ neatAlgorithms/listManipulations/find_longest_increasing_subsequence/main.py
@@ -16,6 +16,18 @@ class branch(list):
             return [e.getTipIndecies() for e in self]
         except TypeError:
             return list(range(len(self)))
+    def get_multiDimIndex(self,index):
+        for i in index:
+            vals = []
+            try:
+                vals.append(self[i])
+            except TypeError:
+                vals.append(self[i].get_multiDimIndex(i))
+    def append_multiDimIndexVal():
+        return
+    def add_branch():
+        return
+
             
 
 def find_LIS(list):
@@ -23,11 +35,20 @@ def find_LIS(list):
     tree.append(list[0])
     tree.depth += 1
     for i,e in enumerate(list):
-        for j,f in enumerate(tree):
-            if e
+        indexs = tree.get_tipIndecies()
+        for j,f in enumerate(tree.get_multiDimIndex(indexs)):
+            if e > f:
+                tree.append_multiDimIndexVal(j,indexs,e)
+            elif e < f:
+                tree.add_branch(j,indexs,e)
+        if i % 10 == 0:
+            tree.cleanup_LIS()
+
+        
 
 
 def main():
+    raise NotImplementedError
     subject = initList()
     LIS = find_LIS(subject)
 
